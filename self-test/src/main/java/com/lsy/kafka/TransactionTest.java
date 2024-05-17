17adac93b929e26ff3486176092a604a2d4dcebf
Linsiyuan9 <154567180+Linsiyuan9@users.noreply.github.com>
2024-05-17 11:29:17 +0800

    temp

--- self-test/src/main/java/com/lsy/kafka/TransactionTest.java
+++ self-test/src/main/java/com/lsy/kafka/TransactionTest.java
@@ -31,7 +31,7 @@ public class TransactionTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-//        t2();
+        t2();
 //        t3();
 //        t4();
     }
@@ -40,42 +40,42 @@ public class TransactionTest {
         // 设置 Kafka 生产者的配置`
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("acks", "all");
-        properties.put("batch.size", 16384);
-        properties.put("linger.ms", 1);
-        properties.put("buffer.memory", 33554432);
+//        properties.put("acks", "all");
+//        properties.put("batch.size", 16384);
+//        properties.put("linger.ms", 1);
+//        properties.put("buffer.memory", 33554432);
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my_tx_id");
-        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        properties.put(ProducerConfig.RETRIES_CONFIG, 10);
+//        properties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my_tx_id");
+//        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.put(ProducerConfig.RETRIES_CONFIG, 10);
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        ProducerRecord<String, String> producerRecord1 = new ProducerRecord<>(
-                "test-topic-1", "1", "sendValue");
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord<String, String> producerRecord1 = new ProducerRecord<>(
+                    "test-topic-1", "1", "sendValue");
+            ProducerRecord<String, String> producerRecord2 = new ProducerRecord<>(
+                    "test-topic-1", "2", "sendValue");
+            ProducerRecord<String, String> producerRecord3 = new ProducerRecord<>(
+                    "test-topic-1", "3", "sendValue");
 
-        ProducerRecord<String, String> producerRecord2 = new ProducerRecord<>(
-                "test-topic-1", "2", "sendValue");
+            producer.initTransactions();
+            producer.beginTransaction();
+            Future<RecordMetadata> recordResult1 = producer.send(producerRecord1);
+            Future<RecordMetadata> recordResult2 = producer.send(producerRecord2);
+            Future<RecordMetadata> recordResult3 = producer.send(producerRecord3);
 
+            System.out.println(Thread.currentThread().getName() + ":start sleep");
+            TimeUnit.SECONDS.sleep(100);
+            System.out.println(Thread.currentThread().getName() + ":end sleep");
 
-        ProducerRecord<String, String> producerRecord3 = new ProducerRecord<>(
-                "test-topic-1", "3", "sendValue");
-
-        producer.initTransactions();
-        producer.beginTransaction();
-        Future<RecordMetadata> recordResult1 = producer.send(producerRecord1);
-        Future<RecordMetadata> recordResult2 = producer.send(producerRecord2);
-        Future<RecordMetadata> recordResult3 = producer.send(producerRecord3);
-
-        System.out.println(Thread.currentThread().getName() + ":start sleep");
-        TimeUnit.SECONDS.sleep(100);
-        System.out.println(Thread.currentThread().getName() + ":end sleep");
-
-        producer.commitTransaction();
-        recordResult1.get();
-        recordResult2.get();
-        recordResult3.get();
+            producer.commitTransaction();
+            recordResult1.get();
+            recordResult2.get();
+            recordResult3.get();
+        }
+        producer.close();
     }
 
     public static void t3() {
